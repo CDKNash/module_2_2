64fce834249a2c2f3244ef9106109335e7db3dfe
CDKNash <cdknash123@gmail.com>
2024-07-05 13:23:23 +0400

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,9 @@
-first = 100
-second = 1000
-third = 10
-if first == second == third:
-    print('3')
-elif first == second != third:
-    print('2')
+num_1 = int(input('Введите число_1: '))
+num_2 = int(input('Введите число_2: '))
+num_3 = int(input('Введите число_3: '))
+if num_1 == num_2 == num_3:
+    print(3)
+elif num_1 == num_2 or num_1 == num_3:
+    print(2)
 else:
-    print('0')
+    print(0)
